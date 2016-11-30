55590604e0e4910a03543940b64cab3c5b3ab84b
matrix <1261305275@qq.com>
2016-11-30 20:16:18 +0800

    modified tree_depth.py

--- tree_depth.py
+++ tree_depth.py
@@ -9,8 +9,8 @@ class Solution:
         if pRoot is None:
             return 0
         
-        child_Depth = max(self.TreeDepth(pRoot.left), self.TreeDepth(pRoot.right))
-        return child_Depth + 1
+        child_depth = max(self.TreeDepth(pRoot.left), self.TreeDepth(pRoot.right))
+        return child_depth + 1
 
 这道题用递归做的话比较简单，注意递归时，怎么抽象。
 一般二叉树的题目将每个节点看做一个抽象单元。
