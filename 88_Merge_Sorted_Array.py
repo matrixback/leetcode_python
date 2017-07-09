ed9563d5ca5c20496fac84385fef7cb190d4745f
matrixback <18302835641@163.com>
2017-07-09 19:39:41 +0800

    add 88

---
+++ 88_Merge_Sorted_Array.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: void Do not return anything, modify nums1 in-place instead.
+        """
+        tail = n + m - 1
+        m -= 1
+        n -= 1
+        while (n>=0) and (m>=0):
+            if nums1[m] > nums2[n]:
+                nums1[tail] = nums1[m]
+                m -= 1
+            else:
+                nums1[tail] = nums2[n]
+                n -= 1
+            tail -= 1
+        if n>=0:
+            nums1[:tail+1] = nums2[:n+1]
